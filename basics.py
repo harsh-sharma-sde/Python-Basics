610dff23924403414207bfc4f99476be6884caf9
Harsh Sharma <harsh.sde.tech@gmail.com>
2025-02-02 19:19:00 +0530

    string

--- basics.py
+++ basics.py
@@ -10,11 +10,13 @@ print (2 + 3) # Addition
 # Integer : 2, 3, 4 -> int
 # Float : 2.3, 3.4, 4.5 -> float
 # Boolean : True, False -> bool
+# Complex : 2 + 3j -> complex
 
 print(type("Hello World")) # <class 'str'>
 print(type(2)) # <class 'int'>
 print(type(2.3)) # <class 'float'>
 print(type(True)) # <class 'bool'>
+print(type(2 + 3j)) # <class 'complex'>
 
 # Variables & Constants
 # Variables : A variable is a container for storing data values.
@@ -43,6 +45,34 @@ print("Hello " + name)
 # Input function always return a string
 
 # Type Casting
-x = int(input("Enter a number: "))
-print(x)
+# x = int(input("Enter a number: "))
+
+x = 5.88
+print(int(x)) # 5
+
+print('Hello' * 3) # HelloHelloHello
+
+# Slicing
+# string[start:end:step]
+
+s = "Hello World"
+print(s[0]) # H
+print(s[0:5]) # Hello
+
+# String Methods
+# len() : Returns the length of the string
+# lower() : Converts a string into lower case
+# upper() : Converts a string into upper case
+# replace() : Replaces a string with another string
+# split() : Splits the string into substrings if it finds instances of the separator
+
+s = "Hello"
+y = "World"
+print(s + " " + y) # Hello World
+print(len(s)) # 5
+print(s.lower()) # hello
+print(s.upper()) # HELLO
+print(s.replace("H", "J")) # Jello
+print(s.split("e")) # ['H', 'llo']
+
 
